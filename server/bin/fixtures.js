f281233711aa26f45f098e28d9c9cd1f828a9b2f
sklyukin <stas.msu@gmail.com>
2015-08-07 23:07:28 +0500

    notify init commit

---
+++ server/bin/fixtures.js
@@ -0,0 +1,25 @@
+let path = require('path'),
+  _ = require('lodash');
+
+let app = require(path.resolve(__dirname, '../server'));
+
+let dataSource = app.dataSources.mongo;
+
+
+function simpleLoad(SingleModelName, pluralModelName) {
+  console.log(`Starting uploading ${pluralModelName}`);
+  const MODELS = require(`./fixtures/${pluralModelName}`);
+  let Model = app.models[SingleModelName];
+  let values = _.values(MODELS);
+  let promises = values.map(data => {
+    return Model.create(data);
+  });
+  return Promise.all(promises)
+    .then(() => console.log(`${pluralModelName}' fixtures are loaded`))
+    .catch((err) => console.log(`Error: ${err}`))
+}
+
+dataSource.automigrate('Everything', () => {
+  simpleLoad('User', 'users')
+    .then(() => simpleLoad('Subscription', 'subscriptions'))
+});
