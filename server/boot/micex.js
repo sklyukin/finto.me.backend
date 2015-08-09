f0c62aa4d290c65456217ed2237a753f0d8da039
sklyukin <stas.msu@gmail.com>
2015-08-10 00:08:27 +0500

    regular request marketdata from micex and save to db

--- server/boot/micex.js
+++ server/boot/micex.js
@@ -1,12 +1,39 @@
 import micexService from '../services/micexService';
 
-export default (server) => {
+export default (app) => {
   micexService.getConstansts()
-    .then((engineAndMarkets) => {
-      micexService.requestLastMarketdata()
-        .then( (data) => {
-            console.log('data received, lenght: ', Object.values(data).length);
-            console.log('USD QUOTE', data.USD000UTSTOM.node.last);
-        });
+    .then(regularRequestData)
+
+  function regularRequestData(){
+    requestAndSaveData().then(() => {
+      let delayMinutes = 1;
+      setTimeout(regularRequestData, delayMinutes * 60 * 1000);
     })
+  }
+
+  function requestAndSaveData() {
+    return micexService.requestLastMarketdata()
+      .then((data) => {
+        let LastData = app.models.LastData;
+        let values = Object.values(data);
+        console.log('Micex data received, length: ', values.length);
+        let lastDatas = values.map((security) => {
+          return new LastData({
+            dataId: security.node.id,
+            value: security.node.last,
+            data: security
+          });
+        })
+        console.log('starting saving Micex data');
+        let promises = lastDatas.map(lastData => LastData.upsert(lastData));
+        return Promise.all(promises);
+      })
+      .then(() => {
+        console.log('Micex data saved');
+      })
+      .catch((error) => {
+        console.log('error occured');
+        console.error(error);
+      });
+  }
 }
